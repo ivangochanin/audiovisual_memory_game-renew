7a6d54c7b2634ed72b155a38f3c28853caeeb073
ivangochanin <ivangochanin@gmail.com>
2021-08-21 01:43:21 +0200

    working game

--- memory.js
+++ memory.js
@@ -1,118 +1,300 @@
-let card = document.querySelectorAll('.card');
-let front = document.querySelectorAll('.front');
-let back = document.querySelectorAll('.back');
-let backMycards = document.querySelectorAll('.myCardsB');
-let myAudio = document.querySelectorAll('.myAudio');
-let switchVisual = document.querySelector('#switchVisual');
-let switchSound = document.querySelector('#switchSound');
-let compareCards = [];
-let theTimer = document.querySelector(".timer");
-let timer = [0,0,0,0];
+import { dataLevel1, dataLevel2, dataLevel3, dataLevel4 } from "./data/data.js";
+let gameWrapper = document.querySelector('#gameWrapper');
+let gameBoard = document.querySelector('#gameBoard');
+let resetGame = document.querySelector('#resetGame');
+let startGame = document.querySelector('#startGame');
+let pauseGame = document.querySelector('#pauseGame');
+let nextLevel = document.querySelector('#nextLevel');
+let currentLevel = document.querySelector('#currentLevel');
+let time = document.querySelector('#timer');
 let openedCards = 0;
+let theme = true;
+let timerOnOff = true;
+let dataPack, cardWrapper, seconds, cardCounter, firstCard, secondCard;
+let level = sessionStorage.level ? parseInt(sessionStorage.level) : 1;
 
-let dataPackage = [
-  {img:"images/card1.png",sound:"images/card1.wav"},
-  {img:"images/card2.png",sound:"images/card2.wav"},
-  {img:"images/card3.png",sound:"images/card3.wav"},
-  {img:"images/card4.png",sound:"images/card4.wav"},
-  {img:"images/card5.png",sound:"images/card5.wav"},
-  {img:"images/card6.png",sound:"images/card6.wav"},
-  {img:"images/card7.png",sound:"images/card7.wav"},
-  {img:"images/card8.png",sound:"images/card8.wav"},
-  {img:"images/card1.png",sound:"images/card1.wav"},
-  {img:"images/card2.png",sound:"images/card2.wav"},
-  {img:"images/card3.png",sound:"images/card3.wav"},
-  {img:"images/card4.png",sound:"images/card4.wav"},
-  {img:"images/card5.png",sound:"images/card5.wav"},
-  {img:"images/card6.png",sound:"images/card6.wav"},
-  {img:"images/card7.png",sound:"images/card7.wav"},
-  {img:"images/card8.png",sound:"images/card8.wav"}
-];
+gameWrapper.style.backgroundColor = "#262627";
 
-back.forEach(random => {
-  let randomData = dataPackage[Math.floor(Math.random() * dataPackage.length)];
-  random.childNodes[1].src = randomData.img;
-  random.childNodes[3].src = randomData.sound;  
-  let randomIndex = dataPackage.indexOf(randomData);
-  dataPackage.splice(randomIndex,1); 
-});
+gsap.to(".infoWrapper", {duration: 2, delay: 1, opacity: 1});
+gsap.to("#timer", {duration: 2, delay: 2, opacity: 1});
+gsap.to(".currentLevel", {duration: 2, delay: 2, opacity: 1});
 
-card.forEach(v => {
-  v.addEventListener('click', rotate); 
-  v.addEventListener('click', playSound);
-  /* v.addEventListener('click', startTimer); */   // every click is new time - F I X !!!!!!!
-});
-
-function rotate() {
-  let x = this.childNodes[1];
-  let y = this.childNodes[3];
-  x.classList.add('front-rotate');
-  y.classList.add('back-rotate');
-  compareCards.push(y);
-  openedCards++;
-  if (openedCards == 2) {
-    console.log(openedCards);
-    card.forEach(v => {
-      setTimeout(function () {
-        v.removeEventListener('click', rotate);
-        v.removeEventListener('click', playSound);
-      }, 100);
-    });
-
-    if (compareCards[0].childNodes[1].src == compareCards[1].childNodes[1].src) {
-      compareCards[0].remove('front-rotate'), compareCards[1].remove('front-rotate');
-      console.log('yeeeee');
-      openedCards = 0;
-      console.log(openedCards);
-    } else {
-      setTimeout(function () {
-        console.log(compareCards[1]);
-        console.log(compareCards);      
-        compareCards[0] = y.classList.replace('back-rotate', 'front-rotate');
-        compareCards[0] = x.classList.replace('front-rotate', 'back-rotate');
-        compareCards[1] = y.classList.replace('back-rotate', 'front-rotate');
-        compareCards[1] = x.classList.replace('front-rotate', 'back-rotate');
-        console.log('nooo');
-        openedCards = 0;
-        console.log(openedCards);
-      }, 1000);
-// setTimeout has to be separat - whole if has to be in extra function
-    }
-    compareCards = [];
-  };
+function addClassAndAtribute(element, className, attribute) {
+	element.classList.add(className);
+	for (let key in attribute) {
+		element.setAttribute(key, attribute[key]);
+	}
 }
 
-function playSound(){
-  let backdiv = this.childNodes[3];
-  let mySound = backdiv.childNodes[3]
-  mySound.play();
-};
+function setLevel() {
+	level === 2 ? cardCounter = 26 : 
+	level === 3 ? cardCounter = 50 : 
+	level === 4 ? cardCounter = 74 : cardCounter = 16;
+	level === 2 ? (dataPack = dataLevel2.concat(dataLevel2)) : 
+	level === 3 ? (dataPack = dataLevel3.concat(dataLevel3)) : 
+	level === 4 ? (dataPack = dataLevel4.concat(dataLevel4)) :
+	(dataPack = dataLevel1.concat(dataLevel1));
+	return level;
+}
 
-switchVisual.addEventListener('click', rotateOnOff);
+function makeGame() {
+	setLevel();
+	dataPack.forEach(() => {
+		let createCards = document.createElement("div");
+		createCards.classList.add("cardWrapper");
+		document.querySelector("#gameBoard").appendChild(createCards);
+		let frontImage = document.createElement("img");
+		let backImage = document.createElement("img");
+		let audio = document.createElement("audio");
+		addClassAndAtribute(backImage, "back", {
+			src: "./images/backB.svg",
+			alt: "backImage",
+		});
+		addClassAndAtribute(frontImage, "front", { src: "", alt: "frontImage" });
+		addClassAndAtribute(audio, "audio", { src: "", preload: "auto" });
+		createCards.append(frontImage, backImage, audio);
+	});
+	randomOrder()
+}
+
+function randomOrder() {
+	cardWrapper = document.querySelectorAll(".cardWrapper");
+	cardWrapper.forEach((i) => {
+		let randomData = dataPack[Math.floor(Math.random() * dataPack.length)];
+		i.childNodes[0].src = randomData.img;
+		i.childNodes[2].src = randomData.sound;
+		let removeData = dataPack.indexOf(randomData);
+		dataPack.splice(removeData, 1);
+		level === 2 ? Object.assign(gameBoard.style, { width: "510px", height: "auto" }) : 
+		level === 3 ? Object.assign(gameBoard.style, { width: "680px", height: "auto"}) : 
+		level === 4 ? Object.assign(gameBoard.style, { width: "840px", height: "auto"}) : 
+		Object.assign(gameBoard.style, { width: "340px", height: "auto" });
+		continueGame()
+	});
+	gsap.to(".cardWrapper", .25, {
+		opacity: 1,
+		yoyo: true, 
+		repeat: 0, 
+		ease: "power1.inOut",
+		delay:1,
+		stagger: {
+			amount: 1, 
+			from: "top"
+		}
+	});
+}
+
+function game() {
+	if(openedCards < 2) {
+		openedCards++ 
+	} 
+	if(openedCards === 1) {
+		firstCard = this
+		firstCard.style.pointerEvents = 'none';
+	}
+	if(openedCards === 2) {
+		secondCard = this
+		secondCard.style.pointerEvents = 'none';
+
+		stopGame()
+		if(firstCard.childNodes[0].currentSrc === secondCard.childNodes[0].currentSrc && firstCard.childNodes[2].currentSrc === secondCard.childNodes[2].currentSrc) {
+			setTimeout(() => {
+				level === 2 ? seconds += 6 : level === 3 ? seconds += 11 : level === 4 ? seconds += 11 : seconds += 6;
+				firstCard.style.visibility = "hidden";
+				secondCard.style.visibility = "hidden";
+				openedCards = 0;
+				continueGame();
+			}, 500);
+			cardCounter = cardCounter - 2
+		} else {
+			setTimeout(() => {
+				firstCard.classList.remove("rotate");
+				secondCard.classList.remove("rotate");
+				openedCards = 0;
+				continueGame()
+			}, 500);
+		}
+		
+	}	
+	setTimeout(() => {
+		if (cardCounter === 0 && level <= 4) {
+			level++;
+			sessionStorage.setItem("level", level);
+			window.location.reload();
+		} 
+		if (cardCounter === 0 && level > 4) {
+			level = 1;
+			sessionStorage.removeItem("level");
+			window.location.reload();
+	} 
+	}, 1000);
+}
+
+function rotateCard() {
+	if (openedCards < 2) {
+		this.classList.add("rotate");
+	} else {
+		cardWrapper.forEach((i) => {
+			i.removeEventListener("click", rotateCard);
+		});
+	}
+}
+
+function playSound() {
+	let sound = this.childNodes[2];
+	sound.currentTime = 0;
+	if (openedCards < 2) {
+		sound ? sound.play() : null;
+	} else {
+		cardWrapper.forEach((i) => {
+			i.removeEventListener("click", playSound);
+		});
+	}
+}
+
+function stopGame() {
+	cardWrapper.forEach((i) => {
+		i.removeEventListener("click", rotateCard);
+		i.removeEventListener("click", playSound);
+		i.removeEventListener("click", game);
+	});
+}
+
+function continueGame() {
+	cardWrapper.forEach((i) => {
+		rotateOnOff();
+		soundOnOff();
+		i.addEventListener("click", game);
+		i.style.pointerEvents = 'auto';
+	});
+}
+
+function themeDarkLight() {
+	let backTheme = document.querySelectorAll('.back')
+     if(switchTheme.checked) {
+		 theme = true;
+		 backTheme.forEach(i => {
+			 i.src = "./images/backW.svg"
+			})
+			gameWrapper.style.backgroundColor = "#ebeaea"; 
+		} else {
+			theme = false;
+			backTheme.forEach(i => {
+				i.src = "./images/backB.svg"
+			})
+		gameWrapper.style.backgroundColor = "#262627";
+	}
+}
+
 function rotateOnOff() {
-  card.forEach(v => {
-  (switchVisual.checked) ? v.removeEventListener('click', rotate) : v.addEventListener('click', rotate); 
- });
-};
+	cardWrapper.forEach((i) => {
+		switchVisual.checked
+			? i.removeEventListener("click", rotateCard)
+			: i.addEventListener("click", rotateCard);
+	});
+}
 
-switchSound.addEventListener('click', soundOnOff);
 function soundOnOff() {
-  card.forEach(s => {
-  (switchSound.checked) ? s.removeEventListener('click', playSound) : s.addEventListener('click', playSound); 
- });
-};
-
-function runTimer() {                       //fix timer to 0:00
-  let currentTime = timer[0] + " : " + timer[1] + " : "  +  timer[2];
-  theTimer.innerHTML = currentTime;
-  timer[3]++;
-
-  timer[0] = Math.floor((timer[3]/100)/60);
-  timer[1] = Math.floor((timer[3]/100) - (timer[0] * 60));
-  timer[2] = Math.floor(timer[3] - (timer[1] * 100) - (timer[0] * 6000));
+	cardWrapper.forEach((i) => {
+		switchSound.checked
+			? i.removeEventListener("click", playSound)
+			: i.addEventListener("click", playSound);
+	});
 }
 
-function startTimer() {
-  setInterval(runTimer, 1000);
+function timeOnOf() {
+	switchTime.checked ?
+	timerOnOff = false : timerOnOff = true
 }
+
+switchTheme.addEventListener("click", themeDarkLight);
+switchVisual.addEventListener("click", rotateOnOff);
+switchSound.addEventListener("click", soundOnOff);
+switchTime.addEventListener("click", timeOnOf);
+
+function timer() {
+	level === 2 ? seconds = 50 : level === 3 ? seconds = 75 : level === 4 ? seconds = 100 : seconds = 25;
+	let countSeconds = setInterval(() => {
+	  if(timerOnOff){
+		seconds--;
+	  }
+	  time.innerHTML = seconds;
+	 
+	  if (seconds === 0) {
+		clearInterval(countSeconds);
+		cardWrapper.forEach((i, index) => {
+			i.removeEventListener("click", rotateCard);
+			i.removeEventListener("click", playSound);
+			i.removeEventListener("click", game);
+			seconds = 1
+			setTimeout(() => {
+				if(i.style.visibility !== "hidden"){
+					i.classList.add("rotate");
+					i.childNodes[2].play();
+				}
+			}, index * 175)
+		});
+	  }
+	  seconds <= 15 ? time.style.color = "orange" : time.style.color = "#262627"
+	}, 1000)
+  }
+
+function reset() {
+	  sessionStorage.removeItem("level");
+	  level = 1;
+	  window.location.reload();
+	}
+	resetGame.addEventListener('click', reset);
+	
+function setNextLevel() {
+	if(level < 4) {
+		level++;
+		sessionStorage.setItem("level", level);
+		window.location.reload();
+	} else {
+		reset()
+	}
+}
+
+function playGameFunc() {
+	switchTime.checked ?
+	timerOnOff = false : timerOnOff = true;
+	cardWrapper.forEach((i) => {
+		switchVisual.checked
+			? i.removeEventListener("click", rotateCard)
+			: i.addEventListener("click", rotateCard);
+		switchSound.checked
+			? i.removeEventListener("click", playSound)
+			: i.addEventListener("click", playSound);
+		i.addEventListener("click", game);
+	});
+	}
+	
+function pauseGameFunc() {
+	  timerOnOff = false;
+	  cardWrapper.forEach((i) => {
+		  i.removeEventListener("click", rotateCard);
+		  i.removeEventListener("click", playSound);
+		  i.removeEventListener("click", game);
+	  });
+  }
+	
+nextLevel.addEventListener('click', setNextLevel);
+pauseGame.addEventListener('click', pauseGameFunc);
+startGame.addEventListener('click', playGameFunc);
+currentLevel.innerHTML = level
+
+function runGame() {
+	makeGame();
+	setTimeout(() => {
+		timer()
+	}, 1000);
+}
+
+runGame()
+pauseGameFunc()
+
+
+
+  
 
