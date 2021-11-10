21d9ee4bf1ee9ec212fd3d4ac9ca924e6b4e06c2
ivangochanin <ivangochanin@gmail.com>
2021-11-10 18:06:09 +0100

    add radio buttons

--- src/modules/accordion.js
+++ src/modules/accordion.js
@@ -1,10 +1,27 @@
-let accordionTrigger = document.querySelectorAll(".accordionTrigger");
+let accordionTrigger = document.querySelectorAll('.accordionTrigger');
 
-function openPanel() {
+/* function openPanel() {
     let panel = this.nextElementSibling;
     panel.style.maxHeight ? panel.style.maxHeight = null : panel.style.maxHeight = panel.scrollHeight + "px";
 }
 
 accordionTrigger.forEach(i => {
-    i.addEventListener('click', openPanel)
-})
+    i.addEventListener('click', openPanel);
+}) */
+
+for (let i = 0; i < accordionTrigger.length; i++) {
+	accordionTrigger[i].addEventListener('click', function () {
+		for (j = 0; j < accordionTrigger.length; j++) {
+			if (j !== i)
+				accordionTrigger[j].nextElementSibling.style.maxHeight = null;
+		}
+		let panel = this.nextElementSibling;
+		panel.style.maxHeight
+			? (panel.style.maxHeight = null)
+			: (panel.style.maxHeight = panel.scrollHeight + 'px');
+	});
+}
+
+
+// Find es6 elegant solution with nested loop outerIndex !== innerIndex
+
