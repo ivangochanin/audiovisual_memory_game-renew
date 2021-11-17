c23a0009bd5b2a079d90036bb87bfa3f86895e54
ivangochanin <ivangochanin@gmail.com>
2021-11-17 19:32:12 +0100

    font & icon theme

--- src/modules/theme.js
+++ src/modules/theme.js
@@ -4,16 +4,26 @@ const theme = () => {
     const sections = document.querySelectorAll('.sections');
     const switchTheme = document.querySelector('#switchTheme');
     const switchThemeText = document.querySelector('#switchThemeText');
-    const containerDark = 'linear-gradient(252.35deg, #161D3B 1.48%, #31397E 49.71%, #6B69D1 97.43%)';
-    const containerLight = 'linear-gradient(252.35deg, #EEEEEE 1.48%, #EEEEEE 49.71%, #EEEEEE 97.43%)';
     const font_default = document.querySelectorAll('.font_default');  
     const font_thin = document.querySelectorAll('.font_thin'); 
+    const front = document.querySelectorAll('.front');
+    const iconWrapper = document.querySelectorAll('.iconWrapper');
+    const gameButtons = document.querySelectorAll('.gameButtons');
+    const switcherSlider = document.querySelectorAll('.switcherSlider');
+    const sliderInputs = document.querySelectorAll('.sliderInputs');
+    const containerDark = 'linear-gradient(252.35deg, #161D3B 1.48%, #31397E 49.71%, #6B69D1 97.43%)';
+    const containerLight = 'linear-gradient(252.35deg, #EEEEEE 1.48%, #EEEEEE 49.71%, #EEEEEE 97.43%)';
     const dark = '#1D1F28';
     const light = '#FFFFFF';
     const darkFont_default = '#E9EDF2';
     const darkFont_thin = '#8a8e96';
     const lightFont_default = '#1D1F28';
     const lightFont_thin = '#70737a';
+    const darkWrapper = '#2D3037';
+    const lightWrapper = '#6E6F71';
+    const blueDark = '#31397E';
+    const blueLight = '#0054E7';
+    const offDark = '#2D3037';
 
     if (!switchTheme.checked) {
         switchThemeText.innerHTML = "Dark";
@@ -24,16 +34,13 @@ const theme = () => {
         }, 25);
 
         Object.assign(container.style, {backgroundImage: containerDark});
-
-        sections.forEach(el => {
-            Object.assign(el.style, {backgroundColor: dark});
-        });
-        font_default.forEach(el => {
-            Object.assign(el.style, {fontFamily: "fontThin", color: darkFont_default});
-        })
-        font_thin.forEach(el => {
-            Object.assign(el.style, {fontFamily: "fontUltralight", color: darkFont_thin});
-        })
+        sections.forEach(el => { Object.assign(el.style, {backgroundColor: dark})});
+        font_default.forEach(el => { Object.assign(el.style, {fontFamily: "fontThin", color: darkFont_default})});
+        font_thin.forEach(el => { Object.assign(el.style, {fontFamily: "fontUltralight", color: darkFont_thin})});
+        front.forEach(el => { Object.assign(el.style, {border: "1px solid #25286b"})});
+        iconWrapper.forEach(el => { Object.assign(el.style, {backgroundColor: darkWrapper})});
+        gameButtons.forEach(el => { Object.assign(el.style, {backgroundColor: darkWrapper})});
+        /* switcherSlider.forEach(el => { Object.assign(el.style, {backgroundColor: blueDark})}); */
 
     } else {
         switchThemeText.innerHTML = "Light";
@@ -43,16 +50,13 @@ const theme = () => {
             })
         }, 25);
         Object.assign(container.style, {backgroundImage: containerLight});
-        
-        sections.forEach(el => {
-            Object.assign(el.style, {backgroundColor: light});
-        });
-        font_default.forEach(el => {
-            Object.assign(el.style, {fontFamily: "fontLight", color: lightFont_default});
-        })
-        font_thin.forEach(el => {
-            Object.assign(el.style, {fontFamily: "fontThin", color: lightFont_thin});
-        })
+        sections.forEach(el => { Object.assign(el.style, {backgroundColor: light})});
+        font_default.forEach(el => { Object.assign(el.style, {fontFamily: "fontLight", color: lightFont_default})});
+        font_thin.forEach(el => { Object.assign(el.style, {fontFamily: "fontThin", color: lightFont_thin})});
+        front.forEach(el => { Object.assign(el.style, {border: "1px solid #0054E7"})});
+        iconWrapper.forEach(el => { Object.assign(el.style, {backgroundColor: lightWrapper})});
+        gameButtons.forEach(el => { Object.assign(el.style, {backgroundColor: lightWrapper})});
+        /* switcherSlider.forEach(el => { Object.assign(el.style, {backgroundColor: blueLight})}); */
     }
 }
 
