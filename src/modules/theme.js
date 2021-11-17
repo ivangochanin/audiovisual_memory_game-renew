b71ff92451c95faf4a5660122535a0ca8dc55f2d
ivangochanin <ivangochanin@gmail.com>
2021-11-17 12:13:15 +0100

    fonts theme

--- src/modules/theme.js
+++ src/modules/theme.js
@@ -5,9 +5,15 @@ const theme = () => {
     const switchTheme = document.querySelector('#switchTheme');
     const switchThemeText = document.querySelector('#switchThemeText');
     const containerDark = 'linear-gradient(252.35deg, #161D3B 1.48%, #31397E 49.71%, #6B69D1 97.43%)';
-    const containerLight = 'linear-gradient(252.35deg, #EEEEEE 1.48%, #EEEEEE 49.71%, #EEEEEE 97.43%)'    
+    const containerLight = 'linear-gradient(252.35deg, #EEEEEE 1.48%, #EEEEEE 49.71%, #EEEEEE 97.43%)';
+    const font_default = document.querySelectorAll('.font_default');  
+    const font_thin = document.querySelectorAll('.font_thin'); 
     const dark = '#1D1F28';
     const light = '#FFFFFF';
+    const darkFont_default = '#E9EDF2';
+    const darkFont_thin = '#8a8e96';
+    const lightFont_default = '#1D1F28';
+    const lightFont_thin = '#70737a';
 
     if (!switchTheme.checked) {
         switchThemeText.innerHTML = "Dark";
@@ -17,11 +23,17 @@ const theme = () => {
             })
         }, 25);
 
-        Object.assign(container.style, {backgroundImage: containerDark, color: light});
+        Object.assign(container.style, {backgroundImage: containerDark});
 
         sections.forEach(el => {
             Object.assign(el.style, {backgroundColor: dark});
         });
+        font_default.forEach(el => {
+            Object.assign(el.style, {fontFamily: "fontThin", color: darkFont_default});
+        })
+        font_thin.forEach(el => {
+            Object.assign(el.style, {fontFamily: "fontUltralight", color: darkFont_thin});
+        })
 
     } else {
         switchThemeText.innerHTML = "Light";
@@ -30,12 +42,17 @@ const theme = () => {
                 i.src = './assets/images/backW.svg';
             })
         }, 25);
-        Object.assign(container.style, {backgroundImage: containerLight, color: dark});
+        Object.assign(container.style, {backgroundImage: containerLight});
         
         sections.forEach(el => {
             Object.assign(el.style, {backgroundColor: light});
         });
-
+        font_default.forEach(el => {
+            Object.assign(el.style, {fontFamily: "fontLight", color: lightFont_default});
+        })
+        font_thin.forEach(el => {
+            Object.assign(el.style, {fontFamily: "fontThin", color: lightFont_thin});
+        })
     }
 }
 
